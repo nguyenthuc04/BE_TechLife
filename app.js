351136735bd87a53a089ff96a17c1b4f1040681e
Quang Vinh <vinh231124@gmail.com>
2024-11-14 18:26:38 +0700

    Home backend

--- app.js
+++ app.js
@@ -39,7 +39,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/uploads', express.static('uploads'));
 
 app.use('/', usersRouter);
 app.use('/', courseRouter);
